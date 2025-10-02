102b0f3bfbb3a0ba788811a073c3185e66912087
Adrián López Gamboa <adrlopgam@alum.us.es>
2025-10-02 11:00:01 +0200

    Añadido protección para ataques Man-in-the-middle y replay

---
+++ cliente.py
@@ -0,0 +1,74 @@
+import socket
+import json
+import secure_utils
+import hashlib
+
+HOST = '127.0.0.1'
+PORT = 11002
+passwd = []
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+
+        print("[Cliente] Conectado al servidor")
+
+        while True:
+            accion = input("Acción (1.register/2.login/3.transaccion/4.logout/5.salir): ")
+            print(accion)
+            if accion == "5":
+                print("[Cliente] Cerrando conexión...")
+                break
+
+            if accion == "1":
+                username = input("Nuevo usuario: ")
+                password = input("Contraseña: ")
+                msg = {"accion": "register", "username": username, "password": password}
+
+            elif accion == "2":
+                username = input("Usuario: ")
+                password = input("Contraseña: ")
+                passwd = []
+                passwd.append(password)
+                msg = {"accion": "login", "username": username, "password": password}
+
+            elif accion == "3":
+                #1º pedimos nonce
+                msg = {"accion":"pet_transaccion"}
+                s.sendall((json.dumps(msg)+ '\n').encode())
+                #2º nos lo envia el servidor
+                resp = s.recv(1024).decode()
+                resp = json.loads(resp)
+                #Vemos si está todo bien
+                status = resp.get("status")
+                #Si es así, es decir que nos hemos logeado, seguimos
+                if status == "OK":
+                    #reconocemos el nonce que nos manda el servidor
+                    nonce = resp.get("nonce")
+                    #creamos el mensaje
+                    data = input("Introduce transacción (CuentaOrigen,CuentaDestino,Cantidad): ")
+                    #sacamos la contraseña, perdon por ser tan bruto para esto
+                    key = passwd[0]
+                    #le sacamos el hash porque es lo que tiene el servidor
+                    key = hashlib.sha256(key.encode()).hexdigest()
+                    #calculamos el hmac
+                    mac = secure_utils.calcula_hmac(key,data, nonce)
+                    #mandamos petición de transacción
+                    msg = {"accion": "transaccion", "data": data, "mac":mac}
+
+            elif accion =="4":
+                msg = {"accion": "logout"}
+
+            else:
+                print("Acción no reconocida.")
+                continue
+            
+            # enviamos el mensaje
+            s.sendall((json.dumps(msg)+ '\n').encode())
+
+            # recibimos respuesta
+            resp = s.recv(1024)
+            print(f"[Cliente] Respuesta del servidor: {resp.decode()}")
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,126 @@
+import socket
+import threading
+import json
+from user_manager import UserManager
+import os
+import secure_utils
+
+SESSIONS_LOG = "sessions.log"
+TRANSACTIONS_LOG = "transactions.log"
+
+user_manager = UserManager()
+sesiones = {}
+transacciones = []
+dic_nonce = {} #diccionario que genera nonce por transacción
+
+HOST = '0.0.0.0'
+PORT = 11002
+
+if os.path.exists(TRANSACTIONS_LOG):
+    os.remove(TRANSACTIONS_LOG)
+
+def log_session_event(evento: str):
+    with open(SESSIONS_LOG, "a") as f:
+        f.write(evento + "\n")
+
+def log_transaction(transaccion: str):
+    with open(TRANSACTIONS_LOG, "a") as f:
+        f.write(transaccion + "\n")
+
+def handle_client(conn, addr):
+    print(f"[+] Cliente con ip {addr} se ha conectado")
+    buffer = ""
+    try:
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                buffer += data.decode()
+
+                while '\n' in buffer:
+
+                    mensaje, buffer = buffer.split('\n',1)
+
+                    if mensaje.strip():
+                        try:
+                            obj = json.loads(mensaje)
+                            print(f"[{addr}] Mensaje de JSON: {obj}")
+
+                            accion = obj.get("accion")
+
+                            if accion == "register":
+                                ok, msg = user_manager.register_user(obj["username"], obj["password"])
+                                resp = {"status": "OK" if ok else "ERROR", "mensaje": msg}
+
+                            elif accion == "login":
+                                ok, msg = user_manager.verify_credenciales(obj["username"], obj["password"])
+                                if ok:
+                                    sesiones[addr] = obj["username"]
+                                    log_session_event(f"[LOGIN] {obj['username']} desde {addr}")
+                                resp = {"status": "OK" if ok else "ERROR", "mensaje": msg}
+                            
+                            #solicitar petición de transacción para generar un nonce
+                            elif accion == "pet_transaccion":
+                                if addr not in sesiones:
+                                    resp = {"status": "ERROR", "mensaje": "Debes iniciar sesión primero"}
+                                else:
+                                    usuario = sesiones[addr] #Sacamos el usuario para guardar su nonce temporal
+                                    nonce = secure_utils.genera_nonce() #Le generamos un nonce
+                                    dic_nonce[usuario] = nonce #Guardamos su nonce
+                                    resp = {"status": "OK","nonce": nonce}
+
+                            elif accion == "transaccion":
+                                transaccion = obj.get("data") #Sacamos la info que envia el usuario
+                                # Ejemplo de lo que hay en transaccion: "CuentaOrigen, CuentaDestino, Cantidad"
+                                usuario = sesiones[addr] #Usuario que realiza la operación
+                                mac = obj.get("mac")
+                                key = user_manager._get_hash_de(usuario)
+                                nonce = dic_nonce[usuario] #Sacamos nonce dado al usuario
+
+                                #Verificar ataque de replay
+                                if not usuario in dic_nonce:
+                                    resp = {"status": "POSIBLE ATAQUE", "mensaje":"Posible ataque de replay, debe avisar al usuario {usuario}"}
+                                #Verificar ataque de Man-in-the-middle (evitar cambios)
+                                elif not secure_utils.verifica_hmac(mac,key,transaccion,nonce):
+                                    resp = {"status": "POSIBLE ATAQUE", "mensaje":"Posible ataque de Man-in-the-middle, deve avisar al usuario {usuario}"}
+                                else:
+                                    dic_nonce.pop(usuario)
+                                    transacciones.append({"usuario": usuario, "transaccion": transaccion})
+                                    log_transaction(f"[TRANSACCION] {usuario}: {transaccion}")
+                                    resp = {"status": "OK", "mensaje": f"Transferencia registrada: {transaccion}"}
+
+                            elif accion == "logout":
+                                if addr in sesiones:
+                                    usuario = sesiones[addr]
+                                    del sesiones[addr]
+                                    log_session_event(f"[LOGOUT] {usuario} desde {addr}")
+                                    resp = {"status": "OK", "mensaje": "Sesión cerrada"}
+                                else:
+                                    resp = {"status": "ERROR", "mensaje": "No estabas logado"}
+
+                            conn.sendall((json.dumps(resp) + "\n").encode())
+
+                        except Exception as e2:
+                            print(f"[!] Ha sucedido un error JSON con {addr}: {e2}")
+
+    except Exception as e:
+        print(f"[!] Ha sucedido un error con  {addr}: {e}")
+    finally:
+        usuario = sesiones.pop(addr, None)
+        log_session_event(f"[LOGOUT] {usuario} (desconexión inesperada) desde {addr}") 
+        print(f"[-] Cliente con ip {addr} se ha desconectado")
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(5)
+        print(f"[Servidor] Escuchando en {HOST}:{PORT}")
+        while True:
+            conn, addr = s.accept()
+            t = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+            t.start()
+
+if __name__ == '__main__':
+    main()
